a6e8bc2d8a64f69ae9ca1d82fb31be23e1e53227
Antonio Arenas <antonioarenas@MacBook-Pro-de-Antonio.local>
2025-04-17 21:28:08 -0500

    tema 4 oporadores de comparacion

---
+++ tema4/1-operadores_de_comparacion.py
@@ -0,0 +1,72 @@
+# Utilizacion  =
+''' 
+# = signo igual simple
+
+miVarible = "hola clase"
+
+
+# == signo igual doble 
+
+34 == 25
+
+respuesta = 34 == 25
+
+print (respuesta)
+
+#compararar operaciones 
+
+respuesta2 = 20 - 10 ==  5 * 2
+print (respuesta2)
+
+# comparar string
+respuesta3 = "blanco" == "Blanco".lower()
+
+print (respuesta3)
+
+
+# comparar un string con un integer
+respuesta4 = 100 == "100"
+
+print (respuesta4)
+
+
+# comparar un float con un integer
+respuesta5 = 100.0== 100
+
+print (respuesta5)
+
+# no es igual
+respuesta6 = 100 != 101
+
+print (respuesta6)
+
+# mayor o igual
+respuesta7 = 5 >= 5
+
+print (respuesta7)
+'''
+
+# Ejercicio 71
+
+num1 = 78
+num2 = 52
+miBool = num1 >= num2
+
+print(miBool)
+print(type(miBool))
+
+#Ejercicio 72 
+num1 = 81**0.5
+num2 = 9
+miBool2 = num1 == num2
+
+print(miBool2)
+print(type(miBool2))
+
+#Ejercicio 72 
+num1 = 856*5
+num2 = 965*4
+miBool3 = num1 != num2
+
+print(miBool3)
+print(type(miBool3))
